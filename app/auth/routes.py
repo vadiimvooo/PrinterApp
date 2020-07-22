c54a7cc529b13f5f870ea5684a224fcf879e0986
Madi <crandquist@gmail.com>
2020-07-22 15:31:02 -0700

    Added register view function and removed remember_me from login view function.

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,6 +2,7 @@ from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 
+from app import db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm
 from app.models import User
@@ -13,11 +14,11 @@ def login():
         return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password.')
             return redirect(url_for('auth.login'))
-        login_user(user, remember=form.remember_me.data)
+        login_user(user)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('main.index')
@@ -29,7 +30,14 @@ def logout():
     logout_user()
     return redirect(url_for('main.index'))
 
-@bp.route('/register')
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now registered!')
+        return redirect(url_for('auth.login'))
     return render_template('register.html', title='Register', form=form)
