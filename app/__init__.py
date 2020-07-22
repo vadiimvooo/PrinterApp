e7e83fde77c66d1d7f9b165187553f09360ca057
Madi <crandquist@gmail.com>
2020-07-21 21:46:20 -0700

    Added database and User model.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from config import Config
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +15,7 @@ def create_app():
 
     bootstrap.init_app(app)
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
