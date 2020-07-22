35abc35f6b16c4fac061110118fddc48d7466e97
Madi <crandquist@gmail.com>
2020-07-21 19:19:39 -0700

    Added login page.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
 
+from app.main.forms import LoginForm
 from app.main import bp
 
 @bp.route('/')
 @bp.route('/index')
 def index():
     return render_template('index.html')
+
+@bp.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign In', form=form)
