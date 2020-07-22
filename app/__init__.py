0286a5e4602da4f85a5e4bacba4d20fdea66e7d3
Madi <crandquist@gmail.com>
2020-07-22 12:40:42 -0700

    Finished user login and started user registration.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from config import Config
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login = LoginManager()
 migrate = Migrate()
 
 def create_app():
@@ -15,7 +17,11 @@ def create_app():
 
     bootstrap.init_app(app)
     db.init_app(app)
+    login.init_app(app)
     migrate.init_app(app, db)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
