c1f856621444863497e8e7de7ab5ed24ea2fa550
Madi <crandquist@gmail.com>
2020-07-23 11:28:48 -0700

    Added add_printer template and very basic route function.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,17 @@
 from flask import render_template
+from flask_login import login_required
 
 from app.main import bp
+from app.main.forms import AddCartridge, RegisterPrinter
 
 
 @bp.route('/')
 @bp.route('/index')
 def index():
     return render_template('index.html')
+
+@bp.route('/add_printer')
+@login_required
+def add_printer():
+    form = RegisterPrinter()
+    return render_template('add_printer.html', title='Add Printer', form=form)
