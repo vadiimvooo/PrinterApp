2adad882f60542fd09770ea058a9dd8b232d7593
Madi <crandquist@gmail.com>
2020-08-03 15:39:55 -0700

    Working on Printer page.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -10,6 +11,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 login = LoginManager()
 migrate = Migrate()
+moment = Moment()
 
 def create_app():
     app = Flask(__name__)
@@ -19,6 +21,7 @@ def create_app():
     db.init_app(app)
     login.init_app(app)
     migrate.init_app(app, db)
+    moment.init_app(app)
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
